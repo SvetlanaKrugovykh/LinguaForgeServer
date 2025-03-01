ab339548d1bf16be0ffd885f38477bd82c166604
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-03-01 14:43:22 +0100

    +++

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 (async () => {
   try {
+    const path = require('path')
+    require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
     await require('../src/services/forTestsCleaningService').cleanTestsService()
   } catch (error) {
     console.error('Error running cleanTestsService:', error)
