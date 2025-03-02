3a8bea6d49a210531c5cafffc52342f69e619073
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-03-02 20:35:32 +0100

    test

--- src/services/wordsService.js
+++ src/services/wordsService.js
@@ -1,20 +1,22 @@
 const db = require('../db/requests')
 const trS = require('./translateService')
-
+const { Translate } = require('@google-cloud/translate').v2
+const { getCredentials } = require('../guards/getCredentials')
+require('dotenv').config()
 
 module.exports.cleanWordTranslates = async function () {
   try {
     const maxTestId = await db.getMaxWordId()
 
-    const total = 400
+    const total = 2
 
     for (let i = maxTestId; i > (maxTestId - total); i--) {
       const row = await db.get1Word(i)
 
       const translations = {}
       translations.en = await trS.translateLWord(row.word, 'pl', 'en')
-      translations.ru = await trS.g_translateText(translations.en, 'en', 'ru')
-      translations.uk = await trS.g_translateText(translations.en, 'en', 'uk')
+      translations.ru = await g1_translateText(translations.en, 'en', 'ru')
+      translations.uk = await g1_translateText(translations.en, 'en', 'uk')
       await db.cleanWord(i, translations)
       console.log(row.word, translations)
     }
@@ -22,5 +24,19 @@ module.exports.cleanWordTranslates = async function () {
   } catch (error) {
     console.error('Error cleaning tests service:', error)
     return null
+  }
+}
+
+async function g1_translateText(text, sourceLanguage, targetLanguage) {
+  try {
+    const CREDENTIALS = getCredentials()
+    const translate = new Translate({ credentials: CREDENTIALS, projectId: CREDENTIALS.project_id })
+    const [translation] = await translate.translate(text, {
+      from: sourceLanguage,
+      to: targetLanguage
+    })
+    return translation
+  } catch (error) {
+    console.error("Error:", error)
   }
 }
